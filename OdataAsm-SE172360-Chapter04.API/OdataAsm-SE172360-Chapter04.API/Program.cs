58763018e1433939576da2a8ec0671fb70eea27b
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-09-18 10:28:00 +0700

    feat: Add Swagger theme and Docker Compose configuration

--- OdataAsm-SE172360-Chapter04.API/OdataAsm-SE172360-Chapter04.API/Program.cs
+++ OdataAsm-SE172360-Chapter04.API/OdataAsm-SE172360-Chapter04.API/Program.cs
@@ -1,25 +1,64 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SwaggerThemes;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
-// Add services to the container.
+// builder.Services.SetupIOCContainer();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty;
+        c.InjectStylesheet("/swagger-ui/custom-theme.css");
+        c.HeadContent = $"<style>{SwaggerTheme.GetSwaggerThemeCss(Theme.UniversalDark)}</style>";
+    });
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
+// try
+// {
+//     app.ApplyMigrations(app.Logger);
+// }
+// catch (Exception e)
+// {
+//     app.Logger.LogError(e, "An problem occurred during migration!");
+// }
 
+app.UseStaticFiles();
+
+app.UseSwagger();
+
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
