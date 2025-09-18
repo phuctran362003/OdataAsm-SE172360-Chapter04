66a890d8fa8c4e4a807e9476440aa6541dba3a8b
Trần Gia Phúc <trangiaphuc362003181@gmail.com>
2025-09-18 09:05:09 +0700

    hehe

---
+++ OdataAsm-SE172360-Chapter04/Commons/PaginationParameter.cs
@@ -0,0 +1,14 @@
+﻿namespace OdataAsm_SE172360_Chapter04.Commons;
+
+public class PaginationParameter
+{
+    private const int maxPageSize = 50;
+    private int _pageSize = 5; // DEPENDENCE ON PROJECT
+    public int PageIndex { get; set; } = 1;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > maxPageSize ? maxPageSize : value;
+    }
+}
---
+++ OdataAsm-SE172360-Chapter04/Utils/ExceptionUtils.cs
@@ -0,0 +1,33 @@
+﻿namespace OdataAsm_SE172360_Chapter04.Utils;
+
+public static class ExceptionUtils
+{
+    /// <summary>
+    ///     Extracts the status code from an exception message or defaults to 500.
+    /// </summary>
+    /// <param name="exceptionMessage">The exception message.</param>
+    /// <returns>An HTTP status code.</returns>
+    public static int ExtractStatusCode(string exceptionMessage)
+    {
+        if (!string.IsNullOrWhiteSpace(exceptionMessage) && exceptionMessage.Length >= 3)
+            if (int.TryParse(exceptionMessage.Substring(0, 3), out var statusCode))
+                return statusCode;
+
+        return 500; // Default status code
+    }
+
+    /// <summary>
+    ///     Creates a standardized error response object.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <returns>An object containing the error response in a standard format.</returns>
+    public static object CreateErrorResponse(string message)
+    {
+        return new
+        {
+            isSuccess = false,
+            message,
+            data = (object)null
+        };
+    }
+}
